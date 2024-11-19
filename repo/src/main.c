af4a2d53ede01af2cfe5448b9d5a9ed5718aaa4d
Quentin Hauuy <qhauuy@k0r4p9.42mulhouse.fr>
2024-11-19 16:32:43 +0100

    blabla

--- repo/src/main.c
+++ repo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:58:26 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/11/19 16:30:36 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/11/19 16:32:07 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,31 +20,10 @@ int	main(int argc, char **argv)
 
 	ft_bzero(&data, sizeof(t_data));
 	parsing(&data, argc, argv);
-	data.ceil_color = data.ceil_rgb->hex;
-	data.floor_color = data.floor_rgb->hex;
-	init_dir(&data);
-	print_map(&data);
-	get_map_dimensions(&data);
-	reverse_map(&data);
-	
 	init_mlx(&data);
-	
-	// int alpha_min = 0;
-	// int alpha_max = 255;
-	int y;
-	y = 0;
-	int alpha;
-	while (y < W_HEIGHT / 2)
-	{
-		alpha = y;
-		printf("%d : %d\n", y, alpha);
-		y++;
-	}
-	
-	
-	// mlx_key_hook(data.mlx, key_hook, &data);
-	// mlx_loop_hook(data.mlx, main_hook, &data);
-	// mlx_loop(data.mlx);
+	mlx_key_hook(data.mlx, key_hook, &data);
+	mlx_loop_hook(data.mlx, main_hook, &data);
+	mlx_loop(data.mlx);
 	free_all(&data);
 	return (0);
 }
