84e73a06497cef21d26f8625d40493548897df23
qhauuy <qhauuy@student.42mulhouse.fr>
2024-12-19 23:05:32 +0100

    blabla

--- repo/src/hook/main_hook.c
+++ repo/src/hook/main_hook.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 13:55:26 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/19 22:54:32 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/12/19 22:59:38 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -346,45 +346,6 @@ static void	check_rotation_keys(t_data *data)
 
 
 
-void	get_pixel_minimap(t_data *data)
-{
-	if (data->xd < 0 || data->yd < 0 || data->xd >= data->map_width
-		|| data->yd >= data->map_height)
-		data->pixel = MAP_COLOR_BG;
-	else if ((data->xd - (int)data->xd) < 0.05
-		|| (data->yd - (int)data->yd) < 0.05)
-		data->pixel = 0x000000FF;
-	else if (data->map[(int)data->xd][(int)data->yd] == '1')
-		data->pixel = MAP_COLOR_WALL;
-	else
-		data->pixel = MAP_COLOR_BG;
-}
-
-void	render_minimap(t_data *data)
-{
-	data->mini_dir_x = -data->dir_y;
-	data->mini_dir_y = data->dir_x;
-	erase_image(data->minimap);
-	data->x = 0;
-	while (data->x < data->minimap->width)
-	{
-		data->y = 0;
-		while (data->y < data->minimap->width)
-		{
-			if ((2 * data->x - data->minimap->width) * (2 * data->x - data->minimap->width) + (2 * data->y - data->minimap->width) * (2 * data->y - data->minimap->width) <= data->mini_w_2)
-			{
-				data->mini_x = (data->x - data->minimap->width / 2.0) * data->mini_step;
-				data->mini_y = (data->y - data->minimap->width / 2.0) * data->mini_step;
-				data->xd = data->pos_x + data->mini_x * data->mini_dir_x + data->mini_y * -data->mini_dir_y;
-				data->yd = data->pos_y + data->mini_x * data->mini_dir_y + data->mini_y * data->mini_dir_x;
-				get_pixel_minimap(data);
-				mlx_put_pixel(data->minimap, data->x, data->y, data->pixel);
-			}
-			data->y++;
-		}
-		data->x++;
-	}
-}
 
 void	main_hook(void *param)
 {
@@ -395,6 +356,6 @@ void	main_hook(void *param)
 	check_movement_keys(data);
 	check_rotation_keys(data);
 	dda(data);
-	if (data->depth_config == 2)
-		render_minimap(data);
+	// if (data->depth_config == 2)
+	// 	render_minimap(data);
 }
--- repo/src/raycasting/draw_ray_map.c
+++ repo/src/raycasting/draw_ray_map.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 18:50:27 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/19 22:55:36 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/12/19 23:02:03 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,10 +42,6 @@ void	draw_ray_minimap(t_data *data)
 			|| data->mini_ray_y < 0 || data->mini_ray_y >= data->minimap->height)
 			break;
 		mlx_put_pixel(data->minimap, data->mini_ray_x, data->mini_ray_y, RAY_COLOR);
-	if (data->x == data->w_width / 2)
-	{
-		printf("%f %f\n", data->mini_ray_x, data->mini_ray_y);
-	}
 		data->ray_x += data->ray_dir_x * data->ray_dir_ratio;
 		data->ray_y += data->ray_dir_y * data->ray_dir_ratio;
 	}
