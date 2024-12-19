4ba208c5ab66c89e9440753b022c63d3c5ac16ad
Quentin Hauuy <quentinhauuy@hotmail.com>
2024-12-19 18:49:26 +0100

    blabla

--- repo/src/hook/main_hook.c
+++ repo/src/hook/main_hook.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 13:55:26 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/19 18:45:55 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/12/19 18:49:25 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,8 @@ void	main_hook(void *param)
 	t_data	*data;
 
 	data = param;
+	if (data->mlx->delta_time < 30)
+		return ;
 	check_movement_keys(data);
 	check_rotation_keys(data);
 	dda(data);
