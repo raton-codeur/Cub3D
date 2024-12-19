8ce2c409d8f9b27aff915570c6018efc06ce3c98
qhauuy <qhauuy@student.42mulhouse.fr>
2024-12-19 21:43:31 +0100

    blabla

--- repo/src/hook/main_hook.c
+++ repo/src/hook/main_hook.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 13:55:26 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/19 21:39:00 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/12/19 21:43:28 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -386,8 +386,8 @@ void	get_pixel_minimap(t_data *data)
 
 void	render_minimap(t_data *data)
 {
-	data->mini_dir_x = data->dir_x;
-	data->mini_dir_y = data->dir_y;
+	data->mini_dir_x = -data->dir_y;
+	data->mini_dir_y = data->dir_x;
 	erase_image(data->minimap);
 	data->x = 0;
 	while (data->x < data->minimap->width)
@@ -395,11 +395,8 @@ void	render_minimap(t_data *data)
 		data->y = 0;
 		while (data->y < data->minimap->width)
 		{
-			// Calculer les coordonnées relatives sur la minimap (centrées autour du joueur)
 			data->mini_x = (data->x - data->minimap->width / 2.0) * data->mini_step;
 			data->mini_y = (data->y - data->minimap->width / 2.0) * data->mini_step;
-
-			// Appliquer la rotation à ces coordonnées pour obtenir (xd, yd)
 			data->xd = data->pos_x + data->mini_x * data->dir_x + data->mini_y * -data->mini_dir_y;
 			data->yd = data->pos_y + data->mini_x * data->dir_y + data->mini_y * data->mini_dir_x;
 
