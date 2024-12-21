9e9c98f458a4057235d9303142fb2eb2e7c266ce
Quentin Hauuy <quentinhauuy@hotmail.com>
2024-12-21 13:04:33 +0100

    blabla

---
+++ repo/src_bonus/main_bonus.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/16 17:58:26 by qhauuy            #+#    #+#             */
+/*   Updated: 2024/12/21 13:04:33 by qhauuy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "parsing.h"
+#include "init_game.h"
+#include "hook.h"
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	ft_bzero(&data, sizeof(t_data));
+	parsing(&data, argc, argv);
+	init_game(&data);
+	mlx_key_hook(data.mlx, key_hook, &data);
+	mlx_loop_hook(data.mlx, main_hook, &data);
+	mlx_mouse_hook(data.mlx, mouse_hook, &data);
+	mlx_cursor_hook(data.mlx, cursor_hook, &data);
+	mlx_loop(data.mlx);
+	free_all(&data);
+	return (0);
+}
+  
