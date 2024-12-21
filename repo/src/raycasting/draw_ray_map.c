34760e42591e00680e43fbdedeb16e79d48e101d
Quentin Hauuy <quentinhauuy@hotmail.com>
2024-12-21 09:50:50 +0100

    blabla

--- repo/src/raycasting/draw_ray_map.c
+++ repo/src/raycasting/draw_ray_map.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 18:50:27 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/21 09:40:37 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/12/21 09:50:48 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,20 +30,20 @@ void	draw_ray_map(t_data *data)
 }
 
 
-void	world_to_minimap(t_data *data, double world_x, double world_y, double *mini_x, double *mini_y)
-{
-	// Décale par rapport à la position du joueur
-	double dx = world_x - data->pos_x;
-	double dy = world_y - data->pos_y;
+// void	world_to_minimap(t_data *data, double world_x, double world_y, double *mini_x, double *mini_y)
+// {
+// 	// Décale par rapport à la position du joueur
+// 	double dx = world_x - data->pos_x;
+// 	double dy = world_y - data->pos_y;
 
-	// Applique la rotation inverse (rotation vers la droite)
-	*mini_x = dx * data->mini_base_x + dy * data->mini_base_y;
-	*mini_y = dx * data->mini_perp_x + dy * data->mini_perp_y;
+// 	// Applique la rotation inverse (rotation vers la droite)
+// 	*mini_x = dx * data->mini_base_x + dy * data->mini_base_y;
+// 	*mini_y = dx * data->mini_perp_x + dy * data->mini_perp_y;
 
-	// Convertit en coordonnées de pixels sur la minimap
-	*mini_x = *mini_x * data->mini_box_size + data->mini_w_2;
-	*mini_y = *mini_y * data->mini_box_size + data->mini_w_2;
-}
+// 	// Convertit en coordonnées de pixels sur la minimap
+// 	*mini_x = *mini_x * data->mini_box_size + data->mini_w_2;
+// 	*mini_y = *mini_y * data->mini_box_size + data->mini_w_2;
+// }
 
 
 void	draw_ray_minimap(t_data *data)
@@ -53,10 +53,14 @@ void	draw_ray_minimap(t_data *data)
 	while (fabs(data->ray_x - data->hit_x) > data->ray_dir_ratio
 		|| fabs(data->ray_y - data->hit_y) > data->ray_dir_ratio)
 	{
-		world_to_minimap(data, data->ray_x, data->ray_y, &data->mini_x, &data->mini_y);
+		data->xd = data->ray_x - data->pos_x;
+		data->yd = data->ray_y - data->pos_y;
+		data->mini_x = data->xd * data->mini_base_x + data->yd * data->mini_base_y;
+		data->mini_y = data->xd * data->mini_perp_x + data->yd * data->mini_perp_y;
+		data->mini_x = data->mini_x * data->mini_box_size + data->mini_w_2;
+		data->mini_y = data->mini_y * data->mini_box_size + data->mini_w_2;
 		if (data->mini_x < 0 || data->mini_x >= data->minimap->width || data->mini_y < 0 || data->mini_y >= data->minimap->width)
-			break;
-			
+			break;			
 		mlx_put_pixel(data->minimap, data->mini_x, data->mini_y, RAY_COLOR);
 		data->ray_x += data->ray_dir_x * data->ray_dir_ratio;
 		data->ray_y += data->ray_dir_y * data->ray_dir_ratio;
