56107782a65f533cb6540ac9b2d897e90cab0dbf
Quentin Hauuy <quentinhauuy@hotmail.com>
2024-12-21 12:35:23 +0100

    blabla

--- repo/src/init_game/init_dimensions.c
+++ repo/src/init_game/init_dimensions.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 00:20:43 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/21 10:50:13 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/12/21 12:33:43 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	init_dimensions(t_data *data)
 		(monitor_width - data->w_width) / 2, \
 		(monitor_height - data->w_height) / 2);
 	if (data->map_width > data->map_height)
-		data->box_size = data->w_width / 3.0 / data->map_width;
+		data->box_size = data->w_width / 2.0 / data->map_width;
 	else
 		data->box_size = data->w_height / 2.0 / data->map_height;
 	if (data->box_size < 10)
