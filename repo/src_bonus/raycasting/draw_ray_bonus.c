9400699f7a8b6b63f5c3c099d0d28bcdfe387d7a
Quentin Hauuy <quentinhauuy@hotmail.com>
2024-12-21 18:48:39 +0100

    blabla

--- repo/src_bonus/raycasting/draw_ray_bonus.c
+++ repo/src_bonus/raycasting/draw_ray_bonus.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 11:10:24 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/21 18:18:04 by qhauuy           ###   ########.fr       */
+/*   Updated: 2024/12/21 18:48:38 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,12 +74,32 @@ void	draw_ray_map_fog(t_data *data)
 		data->ray_x += data->ray_dir_x * data->step_ray_map;
 		data->ray_y += data->ray_dir_y * data->step_ray_map;
 		data->ray_length = (data->ray_x - data->pos_x) * \
-		(data->ray_x - data->pos_x) + (data->ray_y - data->pos_y) * \
-		(data->ray_y - data->pos_y);
+			(data->ray_x - data->pos_x) + (data->ray_y - data->pos_y) * \
+			(data->ray_y - data->pos_y);
 	}
 }
 
 void	draw_ray_minimap_fog(t_data *data)
 {
-	(void)data;
+	data->ray_x = data->pos_x;
+	data->ray_y = data->pos_y;
+	data->ray_length = 0;
+	while (data->ray_length < data->visible_max
+		&& (fabs(data->ray_x - data->hit_x) >= data->step_ray_minimap \
+		|| fabs(data->ray_y - data->hit_y) >= data->step_ray_minimap))
+	{
+		get_coordinates(data);
+		if ((data->mini_xd - data->mini_half) * (data->mini_xd - \
+			data->mini_half) + (data->mini_yd - data->mini_half) * \
+			(data->mini_yd - data->mini_half) > data->mini_ray_limit)
+			return ;
+		data->fog_ratio = 1 - data->ray_length / data->visible_max;
+		mlx_put_pixel(data->minimap, data->mini_xd, data->mini_yd, \
+			COLOR_RAY_FOG + data->fog_ratio * 255);
+		data->ray_x += data->ray_dir_x * data->step_ray_minimap;
+		data->ray_y += data->ray_dir_y * data->step_ray_minimap;
+		data->ray_length = (data->ray_x - data->pos_x) * \
+			(data->ray_x - data->pos_x) + (data->ray_y - data->pos_y) * \
+			(data->ray_y - data->pos_y);
+	}
 }
