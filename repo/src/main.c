c8fb4a95f84311dc0dec738f965cca3e4d56205d
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-01-13 15:19:16 +0100

    blabla

--- repo/src/main.c
+++ repo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 17:58:26 by qhauuy            #+#    #+#             */
-/*   Updated: 2025/01/10 10:14:33 by qhauuy           ###   ########.fr       */
+/*   Updated: 2025/01/13 15:17:16 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,6 @@ int	main(int argc, char **argv)
 	ft_bzero(&data, sizeof(t_data));
 	parsing(&data, argc, argv);
 	init_game(&data);
-	printf("dim : w = %d, h = %d\n", data.map_width, data.map_height);
 	mlx_key_hook(data.mlx, key_hook, &data);
 	mlx_loop_hook(data.mlx, main_hook, &data);
 	mlx_loop(data.mlx);
