1cc394c557c6cc284a0da88695a756afd10ea362
qhauuy <qhauuy@student.42mulhouse.fr>
2025-01-14 14:53:46 +0100

    blabla

--- repo/src_bonus/hook/move_bonus.c
+++ repo/src_bonus/hook/move_bonus.c
@@ -6,7 +6,7 @@
 /*   By: qhauuy <qhauuy@student.42mulhouse.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 18:01:26 by qhauuy            #+#    #+#             */
-/*   Updated: 2024/12/21 16:54:07 by qhauuy           ###   ########.fr       */
+/*   Updated: 2025/01/14 14:44:45 by qhauuy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	move_up(t_data *data)
 {
 	data->new_pos = data->pos_x + STEP_MOVE * data->dir_x;
 	data->i = data->map[(int)data->new_pos][(int)data->pos_y];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_x = data->new_pos;
 		data->map_player->instances[0].x
@@ -24,7 +24,7 @@ void	move_up(t_data *data)
 	}
 	data->new_pos = data->pos_y + STEP_MOVE * data->dir_y;
 	data->i = data->map[(int)data->pos_x][(int)data->new_pos];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_y = data->new_pos;
 		data->map_player->instances[0].y
@@ -36,7 +36,7 @@ void	move_down(t_data *data)
 {
 	data->new_pos = data->pos_x - STEP_MOVE * data->dir_x;
 	data->i = data->map[(int)data->new_pos][(int)data->pos_y];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_x = data->new_pos;
 		data->map_player->instances[0].x
@@ -44,7 +44,7 @@ void	move_down(t_data *data)
 	}
 	data->new_pos = data->pos_y - STEP_MOVE * data->dir_y;
 	data->i = data->map[(int)data->pos_x][(int)data->new_pos];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_y = data->new_pos;
 		data->map_player->instances[0].y
@@ -56,7 +56,7 @@ void	move_left(t_data *data)
 {
 	data->new_pos = data->pos_x + STEP_MOVE * data->dir_y;
 	data->i = data->map[(int)data->new_pos][(int)data->pos_y];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_x = data->new_pos;
 		data->map_player->instances[0].x
@@ -64,7 +64,7 @@ void	move_left(t_data *data)
 	}
 	data->new_pos = data->pos_y - STEP_MOVE * data->dir_x;
 	data->i = data->map[(int)data->pos_x][(int)data->new_pos];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_y = data->new_pos;
 		data->map_player->instances[0].y
@@ -76,7 +76,7 @@ void	move_right(t_data *data)
 {
 	data->new_pos = data->pos_x - STEP_MOVE * data->dir_y;
 	data->i = data->map[(int)data->new_pos][(int)data->pos_y];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_x = data->new_pos;
 		data->map_player->instances[0].x
@@ -84,7 +84,7 @@ void	move_right(t_data *data)
 	}
 	data->new_pos = data->pos_y + STEP_MOVE * data->dir_x;
 	data->i = data->map[(int)data->pos_x][(int)data->new_pos];
-	if (data->i != '1' && data->i != 'C')
+	if (data->i != '1' && data->i != 'D')
 	{
 		data->pos_y = data->new_pos;
 		data->map_player->instances[0].y
